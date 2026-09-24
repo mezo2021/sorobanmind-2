cf10bfa509c631c6c93f98d083e2c3bca095d917
mezo2021 <88048816+mezo2021@users.noreply.github.com>
2026-09-24 05:46:54 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/sorobanmind-2/",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   build: {
     outDir: "dist",
     sourcemap: false,
